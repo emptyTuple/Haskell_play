49eefb49ac4c08d89b89ad1aa362d970933ec190
emptyTuple <kotorov@outlook.com>
2024-09-30 20:06:51 +0700

    isEven added

--- Peano.hs
+++ Peano.hs
@@ -1,48 +1,54 @@
 module Peano where
 
--- succedent
-data Nat = Zero | Succ Nat
+-- successor (succedent)
+data Nat = Nil | Succ Nat
   deriving (Show, Ord)
 
 instance Eq Nat where
   (==) a b =
     case (a, b) of
-      (Zero, Zero)       -> True
+      (Nil, Nil)         -> True
       (Succ a', Succ b') -> a' == b'
       _                  -> False
 
 instance Num Nat where
-  (+) Zero a = a
-  (+) a Zero = a
-  (+) a (Succ b) = Succ (a + b)
-  --(+) (Succ a) b = Succ (a + b) not effective!
-  negate = error "Undefined"
-  (*) Zero _ = Zero
-  (*) _ Zero = Zero
-  (*) a (Succ b) = a + (a * b)
-  --(*) (Succ a) b = b + (a * b)
-  abs x = x
-  signum Zero = Zero
-  signum _  = Succ Zero
-  fromInteger 0 = Zero
-  fromInteger n = Succ (fromInteger (n - 1))
+  (+) Nil a        = a
+  (+) a Nil        = a
+  (+) a (Succ b)   = Succ (a + b)
+  --(+) (Succ a) b = Succ (a + b) not efficient
+  (*) Nil _        = Nil
+  (*) _ Nil        = Nil
+  (*) a (Succ b)   = a + (a * b)
+  fromInteger 0    = Nil
+  fromInteger n    = Succ (fromInteger (n - 1))
+  abs x            = x
+  negate           = error "Undefined"
+  signum Nil       = Nil
+  signum _         = Succ Nil
   
 -- neighbours
 isNeighbour :: Nat -> Nat -> Bool
 isNeighbour a b
   | a == Succ b = True
   | b == Succ a = True
-  | otherwise = False
+  | otherwise   = False
 
 -- not neighbour
 isNotNeighbour :: Nat -> Nat -> Bool
 isNotNeighbour a b 
-  | a == b = False
+  | a == b      = False
   | a == Succ b = False
   | b == Succ a = False
-  |otherwise = True
+  |otherwise    = True
 
 -- pow function
 pow :: Nat -> Nat -> Nat
-pow _ Zero = Succ Zero
+pow _ Nil      = Succ Nil
 pow a (Succ b) = a * pow a b
+
+-- even/odd
+isEven :: Nat -> Bool
+isEven Nil            = True
+isEven (Succ Nil)     = False
+isEven x              = isEven y
+  where Succ (Succ y) = x
