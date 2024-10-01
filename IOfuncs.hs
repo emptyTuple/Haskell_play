559af87dd12114dcdab60ffbf011b1e24aed4825
emptyTuple <kotorov@outlook.com>
2024-10-01 19:43:23 +0700

    IO funcs added

--- IOfuncs.hs
+++ IOfuncs.hs
@@ -1,10 +1,10 @@
 module IOfuncs where
 
+-- | print values from x to y
 printFromTo :: Int -> Int -> IO ()
 printFromTo x y
   | x > y = error "undefined"
   | x == y = print y
   | otherwise = do print x
                    printFromTo (x + 1) y
-
 
