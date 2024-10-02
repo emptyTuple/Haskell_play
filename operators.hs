50c4a922deff4ccadcc6be736b1dc32cdf9f03f9
emptyTuple <kotorov@outlook.com>
2024-10-02 21:04:08 +0700

    priority of operators

--- operators.hs
+++ operators.hs
@@ -4,8 +4,21 @@ summator :: Int -> Int -> Int -> Int
 summator x y z =
     (+) x y + z
 
--- точно также можно использовать обратные кавычки (backticks) для использования
--- функций как операторов: 5 `div` 2
+-- точно также можно использовать backticks для использования
+-- функций в инфиксон стиле: 5 `div` 2
 
- 
-
+{-
+Приоритеты операторов:
+infixr 9  .
+infixl 9  !!
+infixr 8  ^, ^^, **
+infixl 7  *, /, `quot`, `rem`, `div`, `mod`, :%, %
+infixl 6  +, -
+infixr 5  :, ++
+infix  4  ==, /=, <, <=, >=, >, `elem`, `notElem`
+infixr 3  &&
+infixr 2  ||
+infixl 1  >>, >>=
+infixr 1  =<<
+infixr 0  $, $!, `seq`
+-}
