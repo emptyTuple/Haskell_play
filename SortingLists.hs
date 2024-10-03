216e75f4780358b22ff51dfd308ab9869f5e62b8
emptyTuple <kotorov@outlook.com>
2024-10-03 16:58:28 +0700

    sorting started

--- SortingLists.hs
+++ SortingLists.hs
@@ -3,7 +3,7 @@ module SortingLists where
 -- | INSERT SORT
 insertSort :: Ord t => [t] -> [t]
 insertSort [] = []
-insertSort (x:xs) = insert x (insertSort xs)
+insertSort (x:xs) = insert x $ insertSort xs
   where
     insert y [] = [y]
     insert y (z:zs)
