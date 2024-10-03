f09dfd5bfffc1518b6f06df4861db96d517b3c8d
emptyTuple <kotorov@outlook.com>
2024-10-03 22:06:57 +0700

    fib with helper

---
+++ FibonacciWithHelper.hs
@@ -0,0 +1,18 @@
+module FibonacciWithHelper where
+
+-- naive
+fibonacci' :: Integer -> Integer
+fibonacci' n | n == 0    = 0
+            | n == 1    = 1
+            | n > 0     = fibonacci' (n - 1) + fibonacci' (n - 2)
+            | otherwise = undefined 
+
+-- helper
+fibonacci :: Integer -> Integer
+fibonacci = helper 0 1
+  where
+    helper a b x
+      | x == 0 = a
+      | x > 0  = helper b (a + b) (x - 1)
+      | otherwise  = helper (b - a) a (x + 1)
+      
