b76d787e157d812366e7b4959a134a15bbbfe904
emptyTuple <kotorov@outlook.com>
2024-10-03 22:09:03 +0700

    fib with helper

--- FibonacciWithHelper.hs
+++ FibonacciWithHelper.hs
@@ -1,13 +1,13 @@
 module FibonacciWithHelper where
 
--- naive
+-- naive for positive
 fibonacci' :: Integer -> Integer
 fibonacci' n | n == 0    = 0
             | n == 1    = 1
             | n > 0     = fibonacci' (n - 1) + fibonacci' (n - 2)
             | otherwise = undefined 
 
--- helper
+-- with helper, O(n), for all integers
 fibonacci :: Integer -> Integer
 fibonacci = helper 0 1
   where
