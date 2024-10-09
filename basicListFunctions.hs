515e7a469935ac6d844333f43c618538c60136f9
emptyTuple <kotorov@outlook.com>
2024-10-09 23:20:14 +0700

    basic list functions

--- basicListFunctions.hs
+++ basicListFunctions.hs
@@ -54,3 +54,7 @@ zip' :: [a] -> [b] -> [(a,b)]
 zip' _ []          = []
 zip' [] _          = []
 zip' (x:xs) (y:ys) = (x,y) : zip' xs ys
+
+-- unzip function
+unzip' :: [(a,b)] -> ([a], [b])
+unzip' = undefined
