8a59b049cd51b8c3cda8510190428c3b75a2604f
emptyTuple <kotorov@outlook.com>
2024-10-22 21:50:28 +0700

    foldFunctions

---
+++ category.md
@@ -0,0 +1,6 @@
+# Notes
+
+Let $\mathcal{C}$ is a category.
+
+if a function (a morphism) doesn't collapse a domen objects by mapping on a codomen (in other words each domen member has its own single subdomen member) then the function is injective.
+
--- sandbox.hs
+++ sandbox.hs
@@ -1,2 +1,22 @@
+{-# OPTIONS_GHC -Wall #-}  -- Эта прагма добавляет подробности в сообщения об ошибках
+
 module Sandbox where
+
+-- import Data.Char
+
+charToInt :: Char -> Int
+charToInt x = read [x]
+
+data Point = Point Double Double deriving Show
+
+origin :: Point
+origin = Point 0.0 0.0
+
+distanceToOrigin :: Point -> Double
+distanceToOrigin (Point x y) = sqrt (x ** 2 + y ** 2)
+
+distance :: Point -> Point -> Double
+distance (Point x1 y1) (Point x2 y2) = 
+  distanceToOrigin $ Point (x1 - x2) (y1 - y2)
+
 
